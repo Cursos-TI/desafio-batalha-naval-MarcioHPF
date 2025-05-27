8dee1780761576362f286188a9a6fa2524befabb
MarcioHPF <199986094+MarcioHPF@users.noreply.github.com>
2025-05-27 19:16:29 +0000

    Declarei as variáveis

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
+    printf("- - - Batalha Naval - - -\n\n");
+
+    // Declaração de variáveis
+    int tabuleiro[10][10]; // Cria a matriz para o tabuleiro 10x10
+    int l, c; // Cria as variáveis para linhas e colunas do tabuleiro
+
     
 
     return 0;
