15c0a9d1ac1c34e3c71d5679417f36536799d651
MarcioHPF <199986094+MarcioHPF@users.noreply.github.com>
2025-05-27 19:22:14 +0000

    Coloquei os 3s para representarem os navios na matriz

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -13,9 +13,27 @@ int main() {
         for (c = 0; c < 10; c++) {
             tabuleiro[l][c] = 0; // Coloca 0 (água) em todas as posições
         }
-   }
+    }
 
-   
+   // Coloca o primeiro navio na horizontal
+   printf("Posicionando o primeiro navio (horizontal) de tamanho 3.\n");
+   printf("Ele vai da linha 2, coluna 1 até a coluna 3.\n");
+
+   for (c = 0; c < 3; c++) { // O loop vai de 0 a 2 (3 vezes)
+        tabuleiro[2][1 + c] = 3; // Coloca 3 (navio) nas posições
+    }
+    printf("Navio horizontal posicionado!\n\n");
+
+    // Coloca o segundo navio na vertical
+    printf("Posicionando o segundo navio (vertical) de tamanho 3.\n");
+    printf("Ele vai da linha 1, coluna 5 até a linha 3.\n");
+
+    for (l = 0; l < 3; l++) { // O loop vai de 0 a 2 (3 vezes)
+        tabuleiro[1 + l][5] = 3; // Coloca 3 (navio) nas posições
+    }
+    printf("Navio vertical posicionado!\n\n");
+
+    
 
 
 
