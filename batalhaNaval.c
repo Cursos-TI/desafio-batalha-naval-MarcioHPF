9b737d17cb8f56021ce02247e63a7306f799ae6c
MarcioHPF <199986094+MarcioHPF@users.noreply.github.com>
2025-05-27 19:18:33 +0000

    Preenchi a matriz com 0s para representar a água

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,7 +7,18 @@ int main() {
     int tabuleiro[10][10]; // Cria a matriz para o tabuleiro 10x10
     int l, c; // Cria as variáveis para linhas e colunas do tabuleiro
 
-    
+   // Inicia e preenche a matriz com "0" para representar a água
+   printf("Inicializando o tabuleiro \n"); 
+   for (l = 0; l < 10; l++) {
+        for (c = 0; c < 10; c++) {
+            tabuleiro[l][c] = 0; // Coloca 0 (água) em todas as posições
+        }
+   }
+
+   
+
+
+
 
     return 0;
 }
